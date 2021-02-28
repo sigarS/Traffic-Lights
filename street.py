3128b8f662544ad3fb02924796f15963dc9a9025
jeremiahjs <jeremiahjs@live.com>
2021-03-01 00:27:38 +1100

    Create car junction and street class.

---
+++ street.py
@@ -0,0 +1,17 @@
+START = 0
+END = 1
+NAME = 2
+TIME = -1
+class Street:
+    def __init__(self):
+        self.time_to_cross = 0
+        self.start = 0
+        self.end = 0
+        self.name = ""
+
+    def street_creator(self, details):
+
+        self.start = int(details[START])
+        self.end =int(details[END])
+        self.time_to_cross = int(details[TIME])
+        self.name = details[NAME]
